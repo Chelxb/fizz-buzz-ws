6edcb06788d8f5b0bf1524dde694f7db52b4dd09
Andréas CRÉTOIS <39134259+Chelxb@users.noreply.github.com>
2022-07-31 01:04:42 +0200

    Initial commit
    The projet has been initialized with VSCode Quarkus extension.
    The first version of english and french README is written.
    MIT License is selected because of its simplicity.

---
+++ src/test/java/fr/chel/demo/FizzBuzzRessourceIT.java
@@ -0,0 +1,8 @@
+package fr.chel.demo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class FizzBuzzRessourceIT extends FizzBuzzRessourceTest {
+    // Execute the same tests but in packaged mode.
+}
